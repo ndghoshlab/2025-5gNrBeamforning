7a512b822611d0e350c09aab06564b5ecd57a9b8
joshuaroy873 <joshuaroy873@gmail.com>
2025-10-23 14:32:18 -0400

    Default config for /src/stable optim code

--- src/stable/Final5GNRsimulator_optim.m
+++ src/stable/Final5GNRsimulator_optim.m
@@ -15,12 +15,12 @@ clear all, close all, clc;
 % Simulation Parameters
 % -------------------------------------------------------------------------
 
-noSlotsSim                        = 10;         % Number of slots per transmission, min = 1, keep max =< 100.
+noSlotsSim                        = 2;         % Number of slots per transmission, min = 1, keep max =< 100.
 ModOrder                          = "256QAM";    % Modulation order QPSK, 16QAM, 64QAM, 256QAM
-SNRdB                             = [10:5:20];  % SNR in dB, you can input a range too, and have the results for all [0:5:20], etc.
-pmiPrecoding                      = 1;          % If 1 PMI precoder, else SVD
+SNRdB                             = [100];  % SNR in dB, you can input a range too, and have the results for all [0:5:20], etc.
+pmiPrecoding                      = 0;          % If 1 PMI precoder, else SVD
 perfectEstimation                 = false;      % Perfect synchronization and channel estimation
-numIter                           = 1e2         % Number of iterations for this link configuration, number of channels is noSlotsSim * numIter
+numIter                           = 25e3         % Number of iterations for this link configuration, number of channels is noSlotsSim * numIter
 
 %% -------------------------------------------------------------------------
 % Carrier Configuration
