6f2016ee85537b895f637f763739d3c10b45e5c7
joshuaroy873 <joshuaroy873@gmail.com>
2025-10-23 12:10:20 -0400

    temp_plot.m addded to /results

--- results/20251017/temp_plot.m
+++ results/20251017/temp_plot.m
@@ -4,28 +4,28 @@ clc
 SNRdB = 0:5:25;
 
 % % Load PMI results
-% pmi_mod4 = load('Results/simPMI_snrY_nslot2_mod4_niter250e2.mat');
+% pmi_mod4 = load('simPMI_snrY_nslot2_mod4_niter250e2.mat');
 % pmi_mod4 = mean(pmi_mod4.inter_snr);
 
-pmi_mod16 = load('Results/simPMI_snrY_nslot2_mod16_niter250e2.mat');
+pmi_mod16 = load('simPMI_snrY_nslot2_mod16_niter250e2.mat');
 pmi_mod16 = mean(pmi_mod16.inter_snr);
 
 pmi_mod64 = [0.310724679830433,0.234299815930388,0.137534751227131,0.040258231816153,0.009097825747434,0.003189116465863];
 
-pmi_mod256 = load('Results/simPMI_snrY_nslot2_mod256_niter250e2.mat');
+pmi_mod256 = load('simPMI_snrY_nslot2_mod256_niter250e2.mat');
 pmi_mod256 = mean(pmi_mod256.inter_snr);
 
 % % Load SVD results
-% svd_mod4 = load('Results/simSVD_snrY_nslot2_mod4_niter250e2.mat');
+% svd_mod4 = load('simSVD_snrY_nslot2_mod4_niter250e2.mat');
 % svd_mod4 = mean(svd_mod4.inter_snr);
 
-svd_mod16 = load('Results/simSVD_snrY_nslot2_mod16_niter250e2.mat');
+svd_mod16 = load('simSVD_snrY_nslot2_mod16_niter250e2.mat');
 svd_mod16 = mean(svd_mod16.inter_snr);
 
-svd_mod64 = load('Results/simSVD_snrY_nslot2_mod64_niter250e2.mat');
+svd_mod64 = load('simSVD_snrY_nslot2_mod64_niter250e2.mat');
 svd_mod64 = mean(svd_mod64.inter_snr);
 
-svd_mod256 = load('Results/simSVD_snrY_nslot2_mod256_niter250e2.mat');
+svd_mod256 = load('simSVD_snrY_nslot2_mod256_niter250e2.mat');
 svd_mod256 = mean(svd_mod256.inter_snr);
 
 figure
